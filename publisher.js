da259ed9937b1baf2445a87d1fb87fc19896d5cf
Mohirfan <mohirfanbyhaqi@email.unikom.ac.id>
2020-01-12 00:22:19 +0700

    HHH

--- publisher.js
+++ publisher.js
@@ -1,5 +1,5 @@
 var mqtt = require("mqtt");
-var client = mqtt.connect('mqtt://192.168.1.3');
+var client = mqtt.connect('ws://mqtt-carepol.herokuapp.com:80');
 
 client.on('connect', () => {
     setInterval(() => {
